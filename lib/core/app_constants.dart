6e4564e1c3a05a58320b293e27c8d7c914f9e65c
Messaoud_Ben <33179371+massoudsa55@users.noreply.github.com>
2023-05-01 12:16:39 +0100

    initial

---
+++ lib/core/app_constants.dart
@@ -0,0 +1,3 @@
+class AppConstants {
+  static const String urlGraphql = "https://rickandmortyapi.com/graphql";
+}
--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
+import '../../core/app_constants.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -9,9 +11,49 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  HttpLink httpLink = HttpLink("uri");
+  HttpLink httpLink = HttpLink(AppConstants.urlGraphql);
+
+  void _initGraphql() {
+    ValueNotifier<GraphQLClient> client =
+        ValueNotifier(GraphQLClient(link: httpLink, cache: GraphQLCache()));
+    GraphQLProvider(
+      client: client,
+      child: CacheProvider(child: FetchDataScreen()),
+    );
+  }
+
+  @override
+  void initState() {
+    _initGraphql();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
+  }
+}
+
+class FetchDataScreen extends StatelessWidget {
+  const FetchDataScreen({super.key});
+  Future<QueryResult> result(BuildContext context) async {
+    QueryResult query = await GraphQLProvider.of(context)
+        .value
+        .query(QueryOptions(document: gql("""
+query {
+  characters(page: 2, filter: { name: "rick" }) {
+    results {
+      name
+      image
+    }
+  }
+}
+""")));
+    return query;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
